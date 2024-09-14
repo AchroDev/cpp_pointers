0202526e099cd1ef453732c045b28e42237b31d0
AchroDev <achrodev@proton.me>
2024-09-14 17:54:53 -0500

    Added a reference example

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 
 int main()
 {
-    // Pointers are memory addresses, types are used to presume what data is at the address
-    void* ptr = 0; // A void pointer is a typeless memory adress, 0 isn't a valid memory address
-    // void* ptr = NULL; // Same thing as above, NULL is a hash define for 0
-    // void* ptr = nullptr; // Introduced in c++ v.11, this is a null pointer and does the same as the other two above
+    int var = 8;
     
+    // Pointers are memory addresses, types are used to presume what data is at the address
+    void* ptr = &var; // & is a reference to the integer 'var' memory address. 
     
     std::cin.get();
 }
