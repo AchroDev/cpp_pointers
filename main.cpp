0266eb062c8a91eab351eb1340a48ea5644d5705
AchroDev <achrodev@proton.me>
2024-09-14 17:58:12 -0500

    Rewrote data directly at the memory point (seems unsafe)

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main()
     int var = 8;
     
     // Pointers are memory addresses, types are used to presume what data is at the address
-    void* ptr = &var; // & is a reference to the integer 'var' memory address. 
-    *ptr // dereferencing the pointer and going directly to the data    
+    int* ptr = &var; // & is a reference to the integer 'var' memory address. 
+    *ptr = 10; // dereferencing the pointer and going directly to the data    
+    LOG(var);
     
     std::cin.get();
 }
