3f9483011f28c2c5730cff4bf40aaa7f30775a27
AchroDev <achrodev@proton.me>
2024-09-14 17:55:43 -0500

    Added dereferencing example

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     
     // Pointers are memory addresses, types are used to presume what data is at the address
     void* ptr = &var; // & is a reference to the integer 'var' memory address. 
+    *ptr // dereferencing the pointer and going directly to the data    
     
     std::cin.get();
 }
